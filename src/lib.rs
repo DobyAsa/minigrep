8386adf6ee211c4acc7d91975f689cd7a066beae
Doby <hjjhubu@qq.com>
2025-01-07 22:11:59 +0800

    change to funtional verison

--- src/lib.rs
+++ src/lib.rs
@@ -18,26 +18,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
-    let mut strs: Vec<&'a str> = vec![];
-    for line in contents.lines() {
-        if line.contains(query) {
-            strs.push(line);
-        }
-    }
-
-    strs
+    contents.lines()
+            .filter(
+                |line|
+                    line.contains(query))
+            .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut result: Vec<&'a str> = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines()
+            .filter(
+                |line| 
+                    line.to_lowercase().contains(&query))
+            .collect()
 }
 
 #[derive(Debug)]
@@ -48,18 +42,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Can't get the query string.")
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Can't get the file path.")
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-            ignore_case: ignore_case,
-        })
+        Ok(Config { query: query, file_path: file_path, ignore_case: ignore_case })
     }
 }
 
